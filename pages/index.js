3ef76730abac284d8c51537da2d5e247e28b7e9e
bas123s <baskongphop@gmail.com>
2022-02-14 14:04:20 +0700

    GG

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,32 @@
-import Head from "next/head";
-import React,{useEffect} from "react";
-import Image from "next/image";
+import React, { useEffect, useState } from "react";
 import Banner from "../components/banner";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Section1 from "../components/section-1";
 import Section2 from "../components/section-2";
 import Section3 from "../components/section-3";
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import Aos from "aos";
+import "aos/dist/aos.css";
 import Condo from "../components/condo";
-
-
-export default function Home() {
+export default function Index() {
   useEffect(() => {
-    Aos.init({duration : 2000});
-    }, []);
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <div className=" bg-all  min-h-screen">
-     <div data-aos="fade-right"> <Header /></div>
+      <div data-aos="fade-right">
+        {" "}
+        <Header />
+      </div>
       <Banner />
- 
-        <Section1 />
-        {/* <Condo/> */}
-    
-        <Section2/>
-    
+
+      <Section1 />
+      {/* <Condo/> */}
+
+      <Section2 />
+
       {/* <div> <Section3/></div> */}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
