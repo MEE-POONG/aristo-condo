1ff2bcf7e25079af845460d48100c147b1f2b380
bas123s <baskongphop@gmail.com>
2022-02-16 19:43:41 +0700

    pack

--- components/condo.js
+++ components/condo.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import SimpleImageSlider from "react-simple-image-slider";
+
 import Aos from "aos";
 import { IoLocationSharp } from "react-icons/io5";
 import { FaBuilding, FaSwimmingPool } from "react-icons/fa";
